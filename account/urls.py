e8ac5806abfc284bef5e5f6bac0e23298290a2b0
TimeaKovacs86 <kovacs.timea.86@gmail.com>
2020-06-09 17:42:57 +0100

    Created login and logout functions

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import login, registration
+from .views import nav_login, nav_registration, nav_profile, logout
 
 urlpatterns = [
-    url(r'login/', login, name='login'),
-url(r'registration/', registration, name='registration'),
+    url(r'login/', nav_login, name='login'),
+    url(r'registration/', nav_registration, name='registration'),
+    url(r'profile/', nav_profile, name='profile'),
+    url(r'logout/', logout, name='logout'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, reverse
+from account.forms import UserLoginForm
 
 
 # Create your views here.
 
-def login(request):
-    return render(request, "login.html")
+@login_required()
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You have successfully logged out")
+    return redirect(reverse("index"))
 
-def registration(request):
+
+def nav_login(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('profile'))
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('profile'))
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, "login.html", {"login_form": login_form})
+
+
+# def nav_login(request):
+#     return render(request, "login.html")
+
+
+def nav_registration(request):
     return render(request, "registration.html")
+
+
+def nav_profile(request):
+    return render(request, "profile.html")
