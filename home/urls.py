d890a19fc1d4893af764dfbbe306fc5955207d3e
TimeaKovacs86 <kovacs.timea.86@gmail.com>
2020-07-23 21:11:47 +0100

    Create and styling 404 error page and change debug statement

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from .views import index
+from home import views as home_views
+from django.conf.urls import handler404
 
 urlpatterns = [
     url(r'^$', index, name='index'),
 ]
+
+handler404 = home_views.error_404
--- home/views.py
+++ home/views.py
@@ -7,6 +7,9 @@ def index(request):
     return render(request, "index.html")
 
 
+def error_404(request):
+    return render(request, '404.html')
 
 
-
+def error_500(request):
+    return render(request, '404.html')
