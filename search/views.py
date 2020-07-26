1e3796b13bd2f4094304343d048ddcef6b9d0252
TimeaKovacs86 <kovacs.timea.86@gmail.com>
2020-07-26 12:54:57 +0100

    Changed search bar case sensitivity

--- search/views.py
+++ search/views.py
@@ -4,6 +4,6 @@ from products.models import Product
 
 # Create your views here.
 def search(request):
-    products = Product.objects.filter(name__contains=request.GET['search'])
+    products = Product.objects.filter(name__icontains=request.GET['search'])
     is_empty = bool(products)
     return render(request, "products.html", {"products": products, "is_empty": is_empty})
